9e9e0514a654ba57598f9219e1cc7a725843ef0d
Melina <melinafigueroa@tdf.edu.ar>
2022-07-24 15:32:59 -0300

    ejercicio a entregar "form""input"

--- Clase 8 JavaScript/ejercicio a entregar js/script.js
+++ Clase 8 JavaScript/ejercicio a entregar js/script.js
@@ -1,9 +1,19 @@
 "use strict";
-let btn_nombre = document.getElementById("btn-nombre");
-btn_nombre.addEventListener("click",mostrar);
+let btn_datos = document.getElementById("btn-datos");
+btn_datos.addEventListener("click",mostrar);
+
 function mostrar(){
     let nombre = "anonimo";
     let inputNombre = document.getElementById("inputNombre");
+
+    let apellido = "anonimo";
+    let inputApellido = document.getElementById("inputApellido");
+
+    let edad = "anonimo";
+    let inputEdad = document.getElementById("inputEdad");
+
     nombre = inputNombre.value;
-    alert("Bienvenid@ " + nombre);
+    apellido = inputApellido.value;
+    edad = inputEdad.value;
+    alert("Bienvenid@ " + nombre + " " + apellido+ " " + edad+" años");
 }
