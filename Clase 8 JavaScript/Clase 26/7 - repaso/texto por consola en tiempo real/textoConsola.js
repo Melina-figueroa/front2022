76a651b951a27b19ad70164b1b29982277529039
Melina <melinafigueroa@tdf.edu.ar>
2022-08-01 16:35:56 -0300

    cree function mostrarConsola

--- Clase 8 JavaScript/Clase 26/7 - repaso/texto por consola en tiempo real/textoConsola.js
+++ Clase 8 JavaScript/Clase 26/7 - repaso/texto por consola en tiempo real/textoConsola.js
@@ -1,1 +1,22 @@
-alert("Bienvenido");
+"use strict"; //se usa para evitar errores en archivos JS
+let contador = 0;
+let botonContar = document.getElementById("botonContar");
+botonContar.addEventListener("click",contar);
+let botonMostrar = document.getElementById("botonClicks"); //Para obtener el elemento
+botonMostrar.addEventListener("click",cantidadDeClicks); //para esperar el elemento
+let nombre = document.getElementById("mostrarTexto"); //Para obtener el elemento
+nombre.addEventListener("input", mostrarConsola); //para esperar el elemento
+
+
+function contar(){
+    contador ++;
+    console.log(contador);
+}
+function cantidadDeClicks(){
+    alert(contador);
+}
+
+
+function mostrarConsola(){
+    console.log(nombre.value);
+}
